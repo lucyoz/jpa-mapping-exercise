b4dc8ed06eb1b726abe94ce4cbc05c8a79d6e1eb
lucyoz <lucyoz.kwon@gmail.com>
2022-11-23 15:08:25 +0900

    Feat: Publisher추가  - Entity Publisher  - Book Entity에서 OneToOne 단방향 매핑

--- src/main/java/com/example/jpaexercise/domain/Book.java
+++ src/main/java/com/example/jpaexercise/domain/Book.java
@@ -20,4 +20,8 @@ public class Book {
     @ManyToOne
     @JoinColumn(name="author_id")
     private Author author;
+
+    @OneToOne
+    @JoinColumn(name = "publisher_id")
+    private Publisher publisher;
 }
