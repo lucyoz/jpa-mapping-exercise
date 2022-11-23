c7dda2b8f60709e4e915a84fd58fa2c1d2c90a5f
lucyoz <lucyoz.kwon@gmail.com>
2022-11-23 14:10:18 +0900

    Feat: id로 조회하는 기능

--- src/main/java/com/example/jpaexercise/controller/BookController.java
+++ src/main/java/com/example/jpaexercise/controller/BookController.java
@@ -1,10 +1,12 @@
 package com.example.jpaexercise.controller;
 
+import com.example.jpaexercise.domain.Book;
 import com.example.jpaexercise.domain.BookResponse;
 import com.example.jpaexercise.service.BookService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,10 @@ public class BookController {
     @GetMapping("")
     public ResponseEntity<List<BookResponse>> list(Pageable pageable){
         return ResponseEntity.ok().body(bookService.findBooks(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> get(@PathVariable Long id){
+        return ResponseEntity.ok().body(new Book());
     }
 }
